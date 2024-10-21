fbca16b34c97e4a5929f46921b007dfad17b4a35
chiragkscr <chiragksrinivas2003@gmail.com>
2024-10-21 22:25:50 +0530

    updation of recieving messages from dm instead of server

--- apes.py
+++ apes.py
@@ -15,7 +15,7 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 
-Discord_BOT_API_KEY = "MTI5NzEwNTQ5NTQ5NjA2OTE2MQ.G2HGQa.Mn2as2TGwfwDBDZeIVWQkaF3sEq3OrhRh_5FYs"
+Discord_BOT_API_KEY = "MTI5NzEwNTQ5NTQ5NjA2OTE2MQ.G1HMXN.ow0yIoYFNTpVkVHLRoIMmf89xzFO7OSh3Xj4Qo"
 assisstant_id = ASSISSTANT_ID
 thread_id = None
 
@@ -45,47 +45,52 @@ async def on_ready():
 async def on_message(message):
     global thread_id
 
-    if message.author.bot or client.user not in message.mentions:
+    if message.author.bot:
         return 
     
-    user_message = message.content.replace(f"<@{client.user.id}>", "").strip()
 
-    response_message = await message.reply("Typing...")
+    if isinstance(message.channel, discord.DMChannel):
 
-    thread_id = await get_thread_option()
+        user_message = message.content.replace(f"<@{client.user.id}>", "").strip()
+        user = message.author
+        await user.send(f"Hi {user.name}, How can APES assist you here")
+
+        response_message = await user.send("...")
+
+        thread_id = await get_thread_option()
 
 
-    prompts = opclient.beta.threads.messages.create(
-        thread_id=thread_id,
-        role="user",
-        content = user_message
-    )
-    
-    #Running Assisstant
-    run = opclient.beta.threads.runs.create(
-        thread_id = thread_id,
-        assistant_id=assisstant_id
-    )
+        prompts = opclient.beta.threads.messages.create(
+            thread_id=thread_id,
+            role="user",
+            content = user_message
+        )
+        
+        #Running Assisstant
+        run = opclient.beta.threads.runs.create(
+            thread_id = thread_id,
+            assistant_id=assisstant_id
+        )
 
-    while True:
-        try:
-            run = opclient.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run.id)
-            if run.completed_at:
-                messages = opclient.beta.threads.messages.list(
-                    thread_id=thread_id
-                )
-                last_message = messages.data[0]
-                response = last_message.content[0].text.value
-                await response_message.edit(content = response)
+        while True:
+            try:
+                run = opclient.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run.id)
+                if run.completed_at:
+                    messages = opclient.beta.threads.messages.list(
+                        thread_id=thread_id
+                    )
+                    last_message = messages.data[0]
+                    response = last_message.content[0].text.value
+                    await response_message.edit(content = response)
+                    break
+            except Exception as e:
+                logging.error(f"Error occured while retreiving the data : {e}")
                 break
-        except Exception as e:
-            logging.error(f"Error occured while retreiving the data : {e}")
-            break
-        await asyncio.sleep(1)
+            await asyncio.sleep(1)
 
-    run_step = opclient.beta.threads.runs.steps.list(
-        thread_id=thread_id,
-        run_id = run.id
-    )
+    # run_step = opclient.beta.threads.runs.steps.list(
+    #     thread_id=thread_id,
+    #     run_id = run.id
+    # )
 
 client.run(Discord_BOT_API_KEY)
